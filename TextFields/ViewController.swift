abd98945e4d8eb0f618be39402649201710a6a47
Manoli <igielskv@icloud.com>
2020-04-24 22:22:50 +0200

    TextFiled if covered by keyboard moves up as desired by shifting main view. Doesn't work properly on iPhones with Face ID notch and rounded screen corners.

--- TextFields/ViewController.swift
+++ TextFields/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var upperTextField: UITextField!
     @IBOutlet weak var lowerTextField: UITextField!
     
-    var callCounter = 0
+    var activeTextField: UITextField?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,17 +22,46 @@ class ViewController: UIViewController, UITextFieldDelegate {
         upperTextField.delegate = self
         lowerTextField.delegate = self
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeFrame(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        registerForKeyboardNotifications()
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        activeTextField = nil
         textField.resignFirstResponder()
         return true
     }
     
-    @objc func keyboardWillChangeFrame(_ notification: NSNotification) {
-        callCounter += 1
-        print("keyboardWillChangeFrame is being called \(callCounter) times.")
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeTextField = textField
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        
+        let keyboardSpacing: CGFloat = 16.0 // Standard value for spacing between keyboard and textfield
+        
+        guard let userInfo = notification.userInfo,
+            let keyboardFrameValue = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+            else { return }
+        
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        
+        guard let activeTextFieldFrame = activeTextField?.frame else { return }
+        
+        let positionShift = activeTextFieldFrame.origin.y + activeTextFieldFrame.height - keyboardFrame.origin.y + keyboardSpacing
+        
+        if positionShift > 0 {
+            view.frame.origin.y = -positionShift
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        view.frame.origin.y = 0
     }
 }
 
